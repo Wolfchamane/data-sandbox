77bfc587f92a40d3f6f3435a42c79302aab3bf77
Wolfchamane <arturo.mart.diaz@gmail.com>
2018-07-28 15:29:00 +0200

    fix (demo/models/user): lint

--- demo/models/user.js
+++ demo/models/user.js
@@ -1,11 +1,12 @@
 module.exports = {
-    id: {
-        key: true,
-        type: 'String',
-        format: /\d{8}[A-Z]{1}/,
-        random: () => {
+    id              : {
+        key         : true,
+        type        : 'String',
+        format      : /\d{8}[A-Z]{1}/,
+        random      : () =>
+        {
             let value;
-            while(value === undefined)
+            while (value === undefined)
             {
                 const number = Math.trunc(Math.floor(Math.random() * 99999999));
                 const rest = number % 23;
@@ -15,23 +16,26 @@ module.exports = {
                     value = `${number}${letter.charAt(rest)}`;
                 }
             }
+
             return value;
         },
-        validators: [
+        validators  : [
             value => value.length,
-            value => {
+            value =>
+            {
                 const letters = 'TRWAGMYFPDXBNJZSQVHLCKE';
                 const letter = value.match(/[A-Z]/).pop();
+
                 return letters.lastIndexOf(letter) !== -1;
             }
         ]
     },
-    name: {
-        type: 'String',
-        random: true
+    name            : {
+        type        : 'String',
+        random      : true
     },
-    dob: {
-        type: 'Date',
-        random: true
+    dob             : {
+        type        : 'Date',
+        random      : true
     }
 };
