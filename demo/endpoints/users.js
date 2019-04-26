417cd62160a0396f8a736bcd624ea015bf928388
Wolfchamane <arturo.mart.diaz@gmail.com>
2019-04-26 17:28:10 +0200

    wip

--- demo/endpoints/users.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    path : 'users',
-    methods : ['GET', 'POST', 'PUT', 'DELETE'],
-    model: 'user',
-    sample: 10
-};
--- index.js
+++ index.js
@@ -1,36 +1,8 @@
 /* eslint-disable no-console */
 console.log(`data-sandbox Copyright (C) 2018 Arturo Martínez
-This program comes with ABSOLUTELY NO WARRANTY; for details type ????.
+This program comes with ABSOLUTELY NO WARRANTY.
 This is free software, and you are welcome to redistribute it
-under certain conditions; type ??? for details.`);
+under certain conditions.`);
 /* eslint-enable no-console */
 
-const path = require('path');
-const fs = require('fs');
-
-const DataSandboxMicroServer = require('./src/engine/micro-server');
-const DataSandboxModelsEndpoint = require('./src/models/endpoint');
-const DataSandboxModelsInstance = require('./src/models/instance');
-
-/* eslint-disable arrow-body-style */
-const resolve = src =>
-{
-    return path.resolve(__dirname, './', src);
-};
-/* eslint-enable arrow-body-style */
-
-const demoPath = resolve('demo/endpoints');
-fs.readdirSync(demoPath).forEach(endpoint =>
-{
-    /* eslint-disable global-require */
-    endpoint = require(resolve(`demo/endpoints/${endpoint}`));
-    endpoint.model = require(resolve(`demo/models/${endpoint.model}.js`));
-    /* eslint-enable global-require */
-    endpoint.value = [];
-    for (let i = 0; i <= (endpoint.sample - 1); i++)
-    {
-        endpoint.value.push(new DataSandboxModelsInstance(endpoint.model));
-    }
-
-    return new DataSandboxMicroServer('localhost', 3000, new DataSandboxModelsEndpoint(endpoint));
-});
+require('./src/utils/_logger');
